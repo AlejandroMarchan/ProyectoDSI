e0b5819ef466b0afca3c62e01d7ecb022f5214ac
Pablo Berciano <100363964@alumnos.uc3m.es>
2019-04-29 12:49:10 +0200

    Añadir Inicio/Cerrar Sesión a Acerca de

--- App/src/app/acerca-de/acerca-de.page.ts
+++ App/src/app/acerca-de/acerca-de.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController, AlertController, ToastController } from '@ionic/angular';
 import { UsuarioService } from '../services/usuario.service';
+import { LoginPage } from '../login/login.page';
+import { CartaService } from '../services/carta.service';
 
 @Component({
   selector: 'app-acerca-de',
@@ -8,9 +11,43 @@ import { UsuarioService } from '../services/usuario.service';
 })
 export class AcercaDePage implements OnInit {
 
-  constructor(public usuarioService: UsuarioService) { }
+  constructor(public usuarioService: UsuarioService,private cartaService: CartaService, public toastCtrl: ToastController, public modalCtrl: ModalController, public alertCtrl: AlertController) { }
 
   ngOnInit() {
   }
+  async abrirLogin(){
+    let loginModal: HTMLIonModalElement = await this.modalCtrl.create({
+          component: LoginPage
+    });
+    await loginModal.present();
+  }
 
+  async cerrarSesion(){
+    this.cartaService.editar==false;
+    const alert = await this.alertCtrl.create({
+      header: 'Cerrar Sesión',
+      message: '¿Desea cerrar sesión?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+          }
+        }, {
+          text: 'Aceptar',
+          handler: async () => {
+            this.usuarioService.logged = false;
+            const toast = await this.toastCtrl.create({
+              message: '¡Hasta pronto ' + this.usuarioService.username + '!',
+              duration: 2500
+            });
+            toast.present();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
